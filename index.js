69bf36400c83ea02416f874ba30c4507b829681f
Kims-club98 <naxen5611@naver.com>
2025-11-17 12:14:26 +0900

    포트번호 5000번으로 변경

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 5000
 
 app.get('/', (req, res) => {
-  res.send('🚀 Docker + Express 서버가 정상 동작합니다!');
+  res.send(`🚀 Docker + Express 서버가 정상 동작합니다! 포트번호 ${port}`);
 });
 
-app.listen(3000, () => {
-  console.log('서버가 3000번 포트에서 실행 중...');
+app.listen(port, () => {
+  console.log(`서버 ${port}번 포트에서 실행중입니다!`);
 });
